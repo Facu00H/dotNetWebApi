bf9ce19b795d79b4749255f4c9be26ca09265c2a
Facu00H <faisher00@gmail.com>
2023-08-16 15:37:37 -0300

    fix: fix validationAttributes

--- Features/Products/DTOs/CreateProductDTO.cs
+++ Features/Products/DTOs/CreateProductDTO.cs
@@ -7,15 +7,16 @@ namespace WebApplication1.Features.Products.DTOs
     {
         [Required]
         [NotNull]
-        [MinLength(40, ErrorMessage = "you must enter les than 40 characters")]
-        [MaxLength(2, ErrorMessage = "you must enter more than 2 characters")]
+        [MaxLength(40, ErrorMessage = "Name: you must enter less than 40 characters")]
+        [MinLength(2, ErrorMessage = "Name: you must enter more than 2 characters")]
         public string Name { get; set; }
 
-        [Range(0, 100, ErrorMessage = "Please enter a valid number between 0 and 100")]
+        [Range(0, 100, ErrorMessage = "Stock: Please enter a valid number between 0 and 100")]
         public int Stock { get; set; }
 
-        [MaxLength(40, ErrorMessage = "you must enter more than 40 characters")]
-        [MinLength(160, ErrorMessage = "you must enter less than 160 characters")]
+        [MaxLength(160, ErrorMessage = "Description: you must enter less than 160 characters")]
+        [MinLength(40, ErrorMessage = "Description: you must enter more than 40 characters")]
         public string? Description { get; set; }
+
     }
 }
